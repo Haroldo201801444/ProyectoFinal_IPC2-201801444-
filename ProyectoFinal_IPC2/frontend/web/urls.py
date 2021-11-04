c9433ef0fe7ad567e96ac9f11fb34aa2c63ca104
Jose Haroldo Donis Alonso <88604623+Haroldo201801444@users.noreply.github.com>
2021-11-03 21:27:02 -0600

    Delete ProyectoFinal_IPC2/frontend directory

--- ProyectoFinal_IPC2/frontend/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from web import views  #importamos el archivo views del paquete web
-
-urlpatterns = [
-    path('home',views.home,name="Home"),
-    path('reportes',views.reportes,name="Reportes"),
-    path('generacionPdf',views.generacionReportePDF,name="GeneracionPdf")
-]
